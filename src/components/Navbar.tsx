5b3e95d99d7a2a174294de2a27bd12a5b54a6652
012NaveenG <012guptagkp@gmail.com>
2026-01-14 15:44:03 +0530

    add: Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom"
+import Button from "./ui/Button"
+import { Logs, X } from "lucide-react"
+import { useState } from "react"
 
 
 interface NavLink {
@@ -21,27 +24,73 @@ const Navbar = () => {
             href: '#'
         }
     ]
+
+    const [mobileNavOpen, setMobileNavOpen] = useState<false | true>(false)
     return (
-        <header className="sm:max-w-4xl mx-auto">
-            <nav className="flex items-center justify-between">
+        <header className="w-full sm:max-w-6xl mx-auto">
+            <nav className="relative flex items-center justify-between">
                 <Link
                     to={'/'}
-                    className="text-primary font-semibold"
-                >Notes.io</Link>
-                <div className="flex items-center justify-center gap-10">
+                    className="text-primary font-semibold text-lg"
+                >Notes.<span className="italic">io</span></Link>
+                <div className="hidden sm:flex items-center justify-center gap-10">
                     {links.map((link) => (
                         <Link
-                        className="text-blue-900 font-semibold text-sm"
+                            className="text-blue-900 font-semibold text-sm"
                             key={link.title}
                             to={link.href}
                         >{link.title}</Link>
                     ))}
                 </div>
-                <div></div>
+                <div className="hidden sm:block">
+                    <Button className="text-sm">Login</Button>
+                </div>
 
+                <div className="sm:hidden">
+                    {mobileNavOpen ?
+                        <X
+                            className="transition-all duration-900 ease-in-out"
+                            onClick={() => setMobileNavOpen((prev) => !prev)} />
+                        :
+                        <Logs
+                            className="transition-all duration-900 ease-in-out"
+                            onClick={() => setMobileNavOpen((prev) => !prev)} />
+                    }
+                </div>
+                {mobileNavOpen && <MobileNavTray />}
             </nav>
         </header>
     )
 }
 
 export default Navbar
+
+const MobileNavTray = () => {
+    const links: NavLink[] = [
+        {
+            title: 'About',
+            href: '#'
+        },
+        {
+            title: 'Pricing',
+            href: '#'
+        },
+        {
+            title: 'Testimonials',
+            href: '#'
+        }
+    ]
+    return (
+        <div className="absolute  sm:hidden flex flex-col top-10 -left-4 p-2  w-screen  bg-neutral-100 shadow-input transition-all duration-150 ease-linear">
+            {links.map((link) => (
+                <Link
+                    key={link.title}
+                    className="text-xl font-medium my-2 text-neutral-700"
+                    to={'#'}>
+                    {link.title}
+                </Link>
+            ))}
+
+        </div>
+    )
+}
