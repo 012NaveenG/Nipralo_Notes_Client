01e9b20a0dec734c8c2e23296756599947cd82e8
012NaveenG <012guptagkp@gmail.com>
2026-01-14 13:05:51 +0530

    init:project intialized

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,44 @@
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 
+
+interface NavLink {
+    title: string,
+    href: string
+}
 const Navbar = () => {
+
+    const links: NavLink[] = [
+        {
+            title: 'About',
+            href: '#'
+        },
+        {
+            title: 'Pricing',
+            href: '#'
+        },
+        {
+            title: 'Testimonials',
+            href: '#'
+        }
+    ]
     return (
-        <header>
-            <nav>
-                <NavLink
-                 to={''}
-                 className={({isActive})=>('')}
-                 >Notes.io</NavLink>
+        <header className="sm:max-w-4xl mx-auto">
+            <nav className="flex items-center justify-between">
+                <Link
+                    to={'/'}
+                    className="text-primary font-semibold"
+                >Notes.io</Link>
+                <div className="flex items-center justify-center gap-10">
+                    {links.map((link) => (
+                        <Link
+                        className="text-blue-900 font-semibold text-sm"
+                            key={link.title}
+                            to={link.href}
+                        >{link.title}</Link>
+                    ))}
+                </div>
+                <div></div>
+
             </nav>
         </header>
     )
--- src/layouts/IndexLayout.tsx
+++ src/layouts/IndexLayout.tsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar.tsx"
 
 const IndexLayout = () => {
     return (
-        <div className="min-h-screen w-screen bg-linear-to-b from-white via-gray-50  to-blue-200 ">
+        <div className="min-h-screen w-screen bg-linear-to-b from-white via-gray-50  to-blue-200 p-4 ">
             <Navbar />
             <Outlet />
         </div>
