bce3748588405fc4cc28f25b1dbdb67ceced423b
012NaveenG <012guptagkp@gmail.com>
2026-01-16 12:10:24 +0530

    add: registerUser controller

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,8 +1,34 @@
-import axios from "axios"
-import { type LoginForm } from "../types/user.types.ts"
-const loginUser = async (form: LoginForm) => {
-    await axios.post('/api/v1/users/login', form)
+import axios, { AxiosError } from "axios";
+import type { LoginForm, RegisterForm } from "../types/user.types";
+import type { AuthApiResponse } from "../types/api.types";
+const loginUser = async (form: LoginForm): Promise<AuthApiResponse> => {
+    try {
+        const response = await axios.post<AuthApiResponse>(
+            "/api/v1/users/login",
+            form
+        );
 
+        return response.data;
+    } catch (error) {
+        const err = error as AxiosError<any>;
+        throw new Error(err.response?.data?.message || "Login failed");
+    }
+};
+
+
+const registerUser = async (form: RegisterForm): Promise<AuthApiResponse> => {
+    try {
+
+        const response = await axios.post<AuthApiResponse>(
+            "/api/v1/users/register",
+            form
+        );
+
+        return response.data;
+    } catch (error) {
+        const err = error as AxiosError<any>;
+        throw new Error(err.response?.data?.message || "Login failed");
+    }
 }
 
-export { loginUser }
+export { loginUser, registerUser };
